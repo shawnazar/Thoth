4259ae0076365c8a1bf3c6773ede854fbf04ca09
CydiaSA <Shawn@CydiaSolutions.com>
2013-12-27 22:52:52 -0800

    File Versioning - Camera Class
    Versioning of files introduced. Last version archived. This version
    introduces the camera class. Camera class will have actions available
    for the camera on an android device. CameraPreview Class will be next.

--- Previous Versions/Android-12-27-2013/src/com/example/thoth/MainActivity.java
+++ Previous Versions/Android-12-27-2013/src/com/example/thoth/MainActivity.java
@@ -3,15 +3,15 @@ package com.example.thoth;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	static final int REQUEST_IMAGE_CAPTURE = 1;
+
 	/*****************************************************************
 	 * MAIN
 	 ***************************************************************** 
@@ -23,18 +23,13 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		// Viewport object Creation
 		final Button button = (Button) findViewById(R.id.CaptureButton);
 
 		button.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-				
-				captureImageIntent(takePictureIntent);
-				
-				onActivityResult(RESULT_OK, takePictureIntent);
-				
+				captureImageIntent();
 			}
 		});
 
@@ -54,5 +49,15 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-
+	/*****************************************************************
+	 * Capture Image Intent
+	 ***************************************************************** 
+	 * Captures an image
+	 *****************************************************************/
+	private void captureImageIntent() {
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+			startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+		}
+	}
 }
