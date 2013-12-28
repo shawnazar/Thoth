ac949afa3d29558e0eb034c4f62d7bf4ad2ea273
CydiaSA <Shawn@CydiaSolutions.com>
2013-12-27 21:08:25 -0800

    Formatting Issues fixed
    Formatting Issues fixed

--- Android/src/com/example/thoth/MainActivity.java
+++ Android/src/com/example/thoth/MainActivity.java
@@ -12,60 +12,52 @@ public class MainActivity extends Activity {
 
 	static final int REQUEST_IMAGE_CAPTURE = 1;
 
-	
 	/*****************************************************************
-	 *  MAIN
-	 *****************************************************************
-	 * 	On Create
-	 *****************************************************************
-	 *	The on create method is the equivalent of main
+	 * MAIN
+	 ***************************************************************** 
+	 * On Create
+	 ***************************************************************** 
+	 * The on create method is the equivalent of main
 	 *****************************************************************/
-    @Override
-    protected void onCreate(Bundle savedInstanceState) 
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        // Viewport object Creation
-        final Button button = (Button) findViewById(R.id.CaptureButton);
-        
-        button.setOnClickListener(new View.OnClickListener() 
-        {
-            public void onClick(View v) 
-            {
-            	captureImageIntent();
-            }
-        });
-        
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Viewport object Creation
+		final Button button = (Button) findViewById(R.id.CaptureButton);
+
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				captureImageIntent();
+			}
+		});
+
+	}
 
 	/*****************************************************************
 	 * MENU OPTIONS
-	 **
-	 * 	On Create Options Menu
-	 *****************************************************************
-	 *	The on Create options menu creates the menu options for  the user
+	 ** 
+	 * On Create Options Menu
+	 ***************************************************************** 
+	 * The on Create options menu creates the menu options for the user
 	 *****************************************************************/
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) 
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 	/*****************************************************************
-	 * 	Capture Image Intent
-	 *****************************************************************
-	 *	Captures an image
+	 * Capture Image Intent
+	 ***************************************************************** 
+	 * Captures an image
 	 *****************************************************************/
-	private void captureImageIntent() 
-	{
-	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) 
-	    {
-	        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-	    }
+	private void captureImageIntent() {
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+			startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+		}
 	}
 }
