f86e719eb237d54329d88523c0ea32b448f0333d
CydiaSA <Shawn@CydiaSolutions.com>
2014-01-04 00:11:48 -0800

    Unage capture and store in gallary
    needs alot more work, off for the night

--- Android/src/com/thoth/Debug.java
+++ Android/src/com/thoth/Debug.java
@@ -2,14 +2,21 @@ package com.thoth;
 
 import android.util.Log;
 
-public class Constants 
+public class Debug 
 {
+	
+	/* <b>Debug Information</b> */
 	// Output Logcat debug statments
 	public static final boolean DEBUG_STATUS = true;
 	
 	// Tag for LogCat
 	public static final String DEBUG_TAG = "Thoth_Debug";
 	
+	/*
+	 * Log Cat Error
+	 **
+	 *	Creates an output if Debugging is turned on.
+	 */
 	public static void LogCatE(String errorString)
 	{
 		if(DEBUG_STATUS == true)
--- Android/src/com/thoth/MainActivity.java
+++ Android/src/com/thoth/MainActivity.java
@@ -1,15 +1,20 @@
 package com.thoth;
 
-// Our Headers
-import com.thoth.Constants;
 
-// Imports
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 
 
 public class MainActivity extends Activity 
 {
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
@@ -17,10 +22,34 @@ public class MainActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Constants.LogCatE("1 - Program Launched - OnCreate ");
+		 final Button capture = (Button) findViewById(R.id.captureButton);
+		 final Button preview = (Button) findViewById(R.id.previewButton);
+		 final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+		 
+		 final CameraActivity image = new CameraActivity();
+		 
+		 capture.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+            	
+            	
+         	    startActivityForResult(image.getIntent(), 1);
+         	    
+             }
+                 
+         });
+		 
+		 preview.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+            	 imageView.setImageURI(image.getFileURI());
+             }
+                 
+         });
+		 
 		
 		
+			
+
+	    
+
 	}	
-
 }
-
