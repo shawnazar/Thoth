249432cd72a57cb5d9fa1b765c6fac3b1a62dd59
CydiaSA <Shawn@CydiaSolutions.com>
2014-01-03 17:33:19 -0800

    Project name correction
    project name correction

--- Android/gen/com/thoth/BuildConfig.java
+++ Android/gen/com/thoth/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.thoth;
+package com.thoth;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- Android/gen/com/thoth/R.java
+++ Android/gen/com/thoth/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.example.thoth;
+package com.thoth;
 
 public final class R {
     public static final class attr {
@@ -24,8 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int CaptureButton=0x7f080001;
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080004;
+        public static final int captureButton=0x7f080001;
+        public static final int imageView=0x7f080003;
+        public static final int previewButton=0x7f080002;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
